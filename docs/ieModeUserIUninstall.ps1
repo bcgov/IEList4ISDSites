56cbe40b4198cea591c6ff10c1a6d245ae9972c7
Leo <leo.lou@gov.bc.ca>
2022-06-08 15:27:22 -0700

    Create ieModeUserIUninstall.ps1

---
+++ docs/ieModeUserIUninstall.ps1
@@ -0,0 +1,2 @@
+Remove-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Name "InternetExplorerIntegrationSiteList"
+Remove-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Name "InternetExplorerIntegrationLevel"
